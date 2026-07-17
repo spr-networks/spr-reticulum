c20613533c707de5c400e30798a5586fac113726
Alex Rad <alex@supernetworks.org>
2026-07-17 13:37:24 -0700

    Add krun/KVM plugin deployment

--- code/reticulum_plugin.go
+++ code/reticulum_plugin.go
@@ -9,11 +9,11 @@ import (
 	"path/filepath"
 )
 
-var UNIX_PLUGIN_LISTENER = TEST_PREFIX + "/state/plugins/spr-reticulum/socket"
+var UNIX_PLUGIN_LISTENER = TEST_PREFIX + "/run/spr-krun-plugin/spr-reticulum.sock"
 
 // the name of the docker bridge for this plugin (see docker-compose.yml and
 // plugin.json NetworkCapabilities.Interface).
-var gReticulumInterface = "spr-reticulum"
+var gReticulumInterface = "kreticulum0"
 
 var gDaemon = &RNSDaemon{}
 
