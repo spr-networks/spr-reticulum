69eb7fa87fea3950e9e7dbe2950b10063da2c4ee
Alex Rad <alex@supernetworks.org>
2026-07-20 16:45:36 -0700

    Declare KVM plugin runtime

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ if ! sudo nft get element inet filter dhcp_access "{ \"${KRUN_TAP}\" . ${KRUN_MA
 fi
 
 ./build_docker_compose.sh
-docker compose -f docker-compose-krun.yml up -d
+docker compose -f docker-compose-kvm.yml up -d
 
 CONTAINER_IP=
 for _ in $(seq 1 30); do
@@ -69,4 +69,4 @@ curl "http://${API}/firewall/custom_interface" \
 -X 'PUT' \
 --data-raw "{\"SrcIP\":\"${CONTAINER_IP}\",\"Interface\":\"${KRUN_TAP}\",\"Policies\":[\"wan\",\"dns\"],\"Groups\":[\"reticulum\"]}"
 
-docker compose -f docker-compose-krun.yml restart
+docker compose -f docker-compose-kvm.yml restart
